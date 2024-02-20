6b5e226739bd8c802a506123054eda70981cbc10
Léon Frenot <leon.frenot@ens-lyon.fr>
2024-02-20 15:34:29 +0100

    Debut Seq Pattern

--- examples/Patterns/Patterns.v
+++ examples/Patterns/Patterns.v
@@ -14,6 +14,8 @@ Inductive Pat : Type -> Type :=
   (* | Loop: Pat T (graph * graph * graph) *)
   .
 
+(* Head Pattern matching *)
+
 Definition eqb_bid b b' :=
   match b,b' with
     | Name s, Name s' => String.eqb s s'
@@ -39,11 +41,45 @@ end.
 
 Definition find_heads (G: graph) : list (blk * graph) := find_heads_rec G G.
 
+(* Seq Pattern matching *)
+
+Fixpoint subgraph_rec {A} (l1 l2 acc: list A): list (list A*list A) :=
+  match acc with
+    | [] => [(l1, l2)]
+    | a::q => (subgraph_rec (a::l1) l2 q) ++ (subgraph_rec l1 (a::l2) q)
+end.
+
+Definition subgraphs {A} (G: list A) := subgraph_rec [] [] G.
+
+(* Fixpoint subgraph_map_rec {A B} (l1 l2 acc: list A) (f: list A -> list A -> option B): list B :=
+  match acc with
+    | [] => match f l1 l2 with
+      | Some x => [x]
+      | _ => []
+      end
+    | a::q => (subgraph_map_rec (a::l1) l2 q f) ++ (subgraph_map_rec l1 (a::l2) q f)
+end. *)
+
+Fixpoint no_intersect l1 l2: bool := match l2 with
+  | [] => true
+  | a::q => if raw_id_in a l1 then false else no_intersect l1 q
+end.
+
+(* Definition find_seqs_aux (G1 G2: graph): option (graph*graph) := if no_intersect (outputs G2) (inputs G1) then Some (G1, G2) else None. *)
+
+Fixpoint find_seqs_rec (G: list (graph*graph)): list (graph*graph) := match G with
+  | [] => []
+  | (G1,G2)::q => if no_intersect (outputs G2) (inputs G1) then (G1,G2)::(find_seqs_rec q) else find_seqs_rec q
+end.
+
+Definition find_seqs (G: graph): list (graph*graph) := find_seqs_rec (subgraphs G).
+
 Fixpoint MatchAll {S} (P: Pat S) (G: graph) : list S :=
   match P with
     | When _ p f => filter f (MatchAll p G)
     | Head _ p f => flat_map find_heads (map f (MatchAll p G))
     | Graph => [G]
+    | Seq _ p f => flat_map find_seqs (map f (MatchAll p G))
     | _ => []
 end.
 
@@ -114,6 +150,51 @@ Lemma head_correct: forall G G' B, In (B, G') (find_heads G) <-> remove_blk B G 
 Proof.
   intros. apply head_rec_correct.
 Qed.
+
+(* Correction of find_seqs *)
+
+Lemma inputs_cons: forall (G: graph) a, inputs (a::G) = (blk_id a) :: (inputs G).
+Proof.
+  cbn. trivial.
+Qed. 
+
+Lemma list_disjoint_cons_r_iff:
+    forall (A: Type) (a: A) (l1 l2: list A),
+    Coqlib.list_disjoint (l1) (a :: l2) <->
+    (Coqlib.list_disjoint l1 l2 /\ not (In a l1)).
+  Proof.
+    split; intros H.
+    - split; [eapply Coqlib.list_disjoint_cons_right; eauto |].
+      intros abs; eapply H; eauto; constructor; reflexivity.
+    - apply Coqlib.list_disjoint_cons_r; apply H. 
+Qed.
+
+Lemma no_intersect_correct: forall (G1 G2: graph), no_intersect (outputs G2) (inputs G1) = true <-> no_reentrance G1 G2.
+Proof.
+  intros G1 G2. induction G1 as [|a G1 IHG1];split; intro H.
+  - now apply Util.list_disjoint_nil_r.
+  - now trivial.
+  - inversion H. unfold no_reentrance. rewrite inputs_cons. apply Coqlib.list_disjoint_cons_r.
+    * replace (Coqlib.list_disjoint (outputs G2) (inputs G1)) with (no_reentrance G1 G2) by (trivial).
+      apply IHG1. unfold find_seqs_rec. induction (no_intersect (outputs G2) (inputs G1)).
+      trivial. now induction (raw_id_in (blk_id a) (outputs G2)).
+    * now induction (raw_id_in (blk_id a) (outputs G2)).
+  - assert (H0: no_intersect (outputs G2) (inputs (a :: G1)) = true).
+    * rewrite inputs_cons. cbn. induction (raw_id_in (blk_id a) (outputs G2)).
+      + unfold no_reentrance in H. rewrite inputs_cons in H. now apply list_disjoint_cons_r_iff in H as [_ H].
+      + unfold find_seqs_rec in IHG1. unfold no_reentrance in H. rewrite inputs_cons in H. apply list_disjoint_cons_r_iff in H as [H _].
+        unfold no_reentrance in IHG1. now apply IHG1.
+    * now induction (no_intersect (outputs G2) (inputs (a :: G1))).
+Qed.
+
+
+Lemma subgraphs_correct: forall (G1 G: graph), incl G1 G -> exists G2, In (G1, G2) (subgraphs G).
+Proof.
+  intros. induction G1.
+  - intros. exists G. induction G. now left. admit.
+  - apply incl_cons_inv in H as [Ha H]. assert (H0: exists G2 : list blk, In (G1, G2) (subgraphs G)) by (now apply IHG1).
+    destruct H0 as [G2 H0]. exists (remove_blk a G2).
+Abort.
 
 (* Correction of Match *)
 
